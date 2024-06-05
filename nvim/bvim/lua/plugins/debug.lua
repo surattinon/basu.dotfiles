7c74f1a64a4df5388c25a6a27e9052e8e880fa73
surattinon <surattinon.basu@gmail.com>
2024-06-05 12:05:14 +0700

    NVIM v0.0.0

---
+++ nvim/bvim/lua/plugins/debug.lua
@@ -0,0 +1,106 @@
+-- debug.lua
+--
+-- Shows how to use the DAP plugin to debug your code.
+--
+-- Primarily focused on configuring the debugger for Go, but can
+-- be extended to other languages as well. That's why it's called
+-- kickstart.nvim and not kitchen-sink.nvim ;)
+
+return {
+  -- NOTE: Yes, you can install new plugins here!
+  'mfussenegger/nvim-dap',
+  -- NOTE: And you can specify dependencies as well
+  dependencies = {
+    -- Creates a beautiful debugger UI
+    'rcarriga/nvim-dap-ui',
+
+    -- Required dependency for nvim-dap-ui
+    'nvim-neotest/nvim-nio',
+
+    -- Installs the debug adapters for you
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+
+    -- Add your own debuggers here
+    'leoluz/nvim-dap-go',
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    -- NOTE: DAPUI AUTO OPEN
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+
+    --NOTE: DAP KEYMAPS
+    local map = vim.keymap.set
+    map('n', '<leader>dt', dap.toggle_breakpoint, { desc = '[D]ebug [T]oggle Breakpoint' })
+    map('n', '<leader>dc', dap.continue, { desc = '[D]ebug [C]ontinue' })
+    -- map('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+    -- map('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+    -- map('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    map('n', '<leader>dT', function()
+      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    end, { desc = 'Debug: Set Breakpoint' })
+    map('n', '<leader>dx', function()
+      dapui.toggle()
+    end, { desc = 'Toggle Debug UI' })
+
+    local sign = vim.fn.sign_define
+
+    sign('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint', linehl = '', numhl = '' })
+    sign('DapBreakpointCondition', { text = '●', texthl = 'DapBreakpointCondition', linehl = '', numhl = '' })
+    sign('DapLogPoint', { text = '◆', texthl = 'DapLogPoint', linehl = '', numhl = '' })
+
+    require('mason-nvim-dap').setup {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+        'delve',
+      },
+    }
+
+    -- Basic debugging keymaps, feel free to change to your liking!
+
+    -- Dap UI setup
+    -- For more information, see |:help nvim-dap-ui|
+    dapui.setup {
+      -- Set icons to characters that are more likely to work in every terminal.
+      --    Feel free to remove or use ones that you like more! :)
+      --    Don't feel like these are good choices.
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
+
+    -- Install golang specific config
+    require('dap-go').setup {
+      delve = {
+        -- On Windows delve must be run attached or it crashes.
+        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+        detached = vim.fn.has 'win32' == 0,
+      },
+    }
+  end,
+}
---
+++ nvim/bvim/lua/plugins/smoothcursor.lua
@@ -0,0 +1,40 @@
+return {
+  -- NOTE: SMOTH CURSOR
+  {
+    'gen740/SmoothCursor.nvim',
+    config = function()
+      local cursor = require 'smoothcursor'
+      local autocmd = vim.api.nvim_create_autocmd
+
+      cursor.setup {
+        type = 'default', -- Cursor movement calculation method, choose "default", "exp" (exponential) or "matrix".
+
+        cursor = '', -- Cursor shape (requires Nerd Font). Disabled in fancy mode.
+        texthl = 'SmoothCursor', -- Highlight group. Default is { bg = nil, fg = "#FFD400" }. Disabled in fancy mode.
+        linehl = nil, -- Highlights the line under the cursor, similar to 'cursorline'. "CursorLine" is recommended. Disabled in fancy mode.
+      }
+
+      autocmd({ 'ModeChanged' }, {
+        callback = function()
+          local current_mode = vim.fn.mode()
+          if current_mode == 'n' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#8aa872' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == 'v' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == 'V' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == '�' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == 'i' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#668aab' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          end
+        end,
+      })
+    end,
+  },
+}
