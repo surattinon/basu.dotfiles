96e672db996f769baf94ec05207dbd443a90ea5c
surattinon <surattinon.basu@gmail.com>
2024-06-05 20:45:48 +0700

    now can see hidden files

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -10,6 +10,21 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
+    require('neo-tree').setup {
+      event_handlers = {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          vim.opt_local.relativenumber = true
+        end,
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+        },
+      },
+    }
     local map = vim.keymap.set
     local silent = { silent = true }
 
