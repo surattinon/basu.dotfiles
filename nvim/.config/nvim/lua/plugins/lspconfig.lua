aaf9164e3bc4efe17689c0118375a85fc54f0172
surattinon <surattinon.basu@gmail.com>
2024-06-05 20:45:16 +0700

    add ensure-installed for LSPs formatters and linters

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -210,8 +210,27 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        -- NOTE: LSPs
         'gopls',
+        'pyright',
+        'clangd',
+        'tsserver',
+        'lua_ls',
+        'rust_analyzer',
+
+        -- NOTE: Formatters
+        'stylua',
+        'shfmt',
+        'goimports',
+        'gofumpt',
+        'goimports-reviser',
+        'prettierd',
+        'prettier',
+        'google-java-format',
+
+        -- NOTE: Linters
+        'eslint_d',
+        'checkstyle',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
